eba37536b06837defa2b6db74119513fca82802f
Rafael Miceli <rafael.miceli@hotmail.com>
2013-10-23 20:43:59 -0200

    Client App consuming a WCF service

--- SolutionMcts70-513/New-Studies/WcfClients/ConsoleAppClient/Program.cs
+++ SolutionMcts70-513/New-Studies/WcfClients/ConsoleAppClient/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleAppClient.EvaluateServiceReference;
 
 namespace ConsoleAppClient
 {
@@ -10,6 +12,30 @@ namespace ConsoleAppClient
     {
         static void Main(string[] args)
         {
+            var cf = new ChannelFactory<IEvalServiceChannel>("End4");
+
+            var service = cf.CreateChannel();
+
+            var newEval = new Eval
+            {
+                Comments = "Testing",
+                Submitter = "Rafael",
+                Timesent = DateTime.Now
+            };
+
+            service.SubmitEval(newEval);
+            service.SubmitEval(newEval);
+
+            var evals = service.GetEvals();
+
+            foreach (var eval in evals)
+            {
+                Console.WriteLine(eval.Submitter +  " " + eval.Comments);
+            }
+
+            service.Close();
+
+            Console.ReadLine();
         }
     }
 }
